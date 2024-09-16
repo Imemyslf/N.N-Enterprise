7965ad741b229287ad70bcb2af5429c2c89da9ce
Imemyslf <kishanbro9666@gmail.com>
2024-09-16 07:05:33 +0530

    Added billings-backend folder

--- Invoice-App/billings/src/Pages/AddCompany.js
+++ Invoice-App/billings/src/Pages/AddCompany.js
@@ -5,24 +5,20 @@ import { Label } from "../Components/Label";
 
 const Company = () => {
     const [companyName,setCompanyName] = useState("");
+    const [companyGST,setCompanyGST] = useState("");
+    const [companyEmail,setCompanyEmail] = useState("");
+    const [companyStateCode,setCompanyStateCode] = useState("");
     const [name,setName] = useState("");
 
-    
-    const handleCompanyName = (e) => {
-        setCompanyName(e.target.value);
-    }
 
     const displayName = (e) => {
         e.preventDefault();
-        setName(companyName);
-
+        const obj = [companyName,companyEmail,companyGST,companyStateCode]
+        setName(obj);
     }
 
     return(
         <>
-        {/* <div onClick={handlecount}>
-            <h1>{count}</h1>
-        </div> */}
             
             <h1>{name}</h1>
                 <form action="">
@@ -31,8 +27,30 @@ const Company = () => {
                         type="text" 
                         placeholder="Enter Company's name"
                         value = {companyName}
-                        change = {handleCompanyName}
-                    /><br/><br/>
+                        change = {e => setCompanyName(e.target.value)}
+                    /><br/>
+                    <Label name="Company's GST Nos:-"/> <br/>
+                    <Input 
+                        type="text" 
+                        placeholder="Enter Company's GST Nos"
+                        value = {companyGST}
+                        change = {e => setCompanyGST(e.target.value)}
+                    /><br/>
+                    <Label name="Company's Email:-"/> <br/>
+                    <Input 
+                        type="text" 
+                        placeholder="Enter Company's name"
+                        value = {companyEmail}
+                        change = {e => setCompanyEmail(e.target.value)}
+                    /><br/>
+                    <Label name="Company's GST Nos:-"/> <br/>
+                    <Input 
+                        type="text" 
+                        placeholder="Enter Company's name"
+                        value = {companyStateCode}
+                        change = {e => setCompanyStateCode(e.target.value)}
+                    />
+                    <br/><br/>
                     <button onClick={displayName}>Click Me!!</button>
                 </form>
         </>  
