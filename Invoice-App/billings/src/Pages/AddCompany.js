20763b7092ef98530234af305c3f6af6643f3b19
Imemyslf <kishanbro9666@gmail.com>
2024-09-16 18:45:59 +0530

    Addedc changes

--- Invoice-App/billings/src/Pages/AddCompany.js
+++ Invoice-App/billings/src/Pages/AddCompany.js
@@ -47,7 +47,6 @@ const Company = () => {
         // onSubmit={handleSubmit}
       >
         <h3 className="form-header">ADD NEW COMPANY</h3>
-<<<<<<< HEAD
         <Col sm="12">
           <Row className="mb-4">
             <Form.Group as={Col} controlId="formGridCompanyName">
@@ -106,30 +105,6 @@ const Company = () => {
         <Button variant="primary" onClick={handleSubmitt}>
           SUBMIT
         </Button>
-=======
-        <Row className="mb-4">
-          <Form.Group as={Col} controlId="formGridCompanyName">
-            <Form.Label>Company name</Form.Label>
-            <Form.Control type="text" placeholder="N.N Enterprises" change={(e) => setCompanyName(e.target.value)} required />
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridCompanyGSTNo">
-            <Form.Label>Company GSTIN no.</Form.Label>
-            <Form.Control type="text" placeholder="30ABXYP2839XYZ" change={(e) => setCompanyGST(e.target.value)} required />
-          </Form.Group>
-        </Row>
-        <Row className="mb-4">
-          <Form.Group as={Col} md="6" controlId="formGridCompanyEmail">
-            <Form.Label>Company email</Form.Label>
-            <Form.Control type="email" placeholder="Maysurtraders-goa@gmail.com" change={(e) => setCompanyEmail(e.target.value)} required />
-          </Form.Group>
-          <Form.Group as={Col} md="4" controlId="formGridStateCode">
-            <Form.Label>Company state code</Form.Label>
-            <Form.Control type="number" placeholder="35" change={(e) => setCompanyStateCode(e.target.value)} required />
-          </Form.Group>
-        </Row>
-        
-        <Button variant="primary" type="submit">SUBMIT</Button>
->>>>>>> f79a7804fa433079165e0ec484f2077ea96ab153
       </Form>
 
       {/* <h1>{name}</h1>
