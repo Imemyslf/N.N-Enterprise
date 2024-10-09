de39a4aad26286f22d63777a5175c830f1136def
Kishan-A-Sharma <112005565+Imemyslf@users.noreply.github.com>
2024-10-09 06:45:21 +0530

    Update database.js

--- Invoice-App/billings-backend/database.js
+++ Invoice-App/billings-backend/database.js
@@ -7,7 +7,7 @@ async function connectToDb(cb) {
   console.log(`${process.env.MONGODB_USER} ${process.env.MONGODB_PASS}`);
   // const client = new MongoClient(`mongodb://127.0.0.1:27017`);
   const client = new MongoClient(
-    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.p8yp8.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.p8yp8.mongodb.net/dCompany?retryWrites=true&w=majority&appName=Cluster0`
   );
   await client.connect();
 
